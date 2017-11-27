c7f9fa1ba5bf4b28b9bae93846bfccab76aa8fd0
denixander <34031752+denixander@users.noreply.github.com>
2017-11-27 20:59:56 +0700

    Add files via upload

---
+++ development dpmptspbs HTML5/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	
+	var WinHeight = $('.billboard').height() - 100;
+	var BgColor = 'rgba(255, 255, 255, .1)';
+	var DarkBgColor = 'rgba(238, 169, 89, .9)';
+	
+	$(window).scroll(function () {
+	    if ($(window).scrollTop() < WinHeight) {
+	    	$("header").css("background-color", BgColor);
+	    } else {
+	    	$("header").css("background-color", DarkBgColor);
+			
+			<script type="text/javascript">
+function googleTranslateElementInit() {
+new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+	    }
+	})
+
+});
